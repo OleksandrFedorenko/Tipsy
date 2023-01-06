d304c47c2e4a2530931ca6f51a97047e4ca0f3ae
Summer <afedorenko02@gmail.com>
2023-01-06 18:53:49 +0200

    segue was added

---
+++ Tipsy/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  Tipsy
+//
+//  Created by Олександр Федоренко on 06.01.2023.
+//
+
+import UIKit
+import Foundation
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
