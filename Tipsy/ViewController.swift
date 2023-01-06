25ccfa73b00c2850f2a68d4ffd2a72fb19f4030c
Summer <afedorenko02@gmail.com>
2023-01-06 14:16:35 +0200

    Template of project has been created

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Олександр Федоренко on 06.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //adfa
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
